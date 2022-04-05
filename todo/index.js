598214fa03a99e19b8e39d9f17e168bf5bd82e8d
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-04-05 17:06:50 +0300

    Add task delete, change, store

--- todo/index.js
+++ todo/index.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('add-todo');
 form.addEventListener('submit', onSubmit);
 
+const todoList = document.querySelector('#render-todos');
 const todoText = document.querySelector('[name="todo-text"]');
-
 function onSubmit(event) {
   event.preventDefault();
   
@@ -12,13 +12,82 @@ function onSubmit(event) {
   }
 
   // add new tag to list
-  addTask(todoText.value);
+  addTaskToHTML(todoText.value);
+  addTaskObj(todoText.value);
   todoText.value = '';
 }
 
-const todoList = document.querySelector('#render-todos');
-function addTask(value) {
+/* ======================== */
+
+const tasks = [];
+function initTasks() {
+  const string = localStorage.getItem('tasks');
+
+  if (string) {
+    const taskArr = JSON.parse(string);
+    tasks.push(...taskArr);
+  }
+
+  for(const task of tasks) {
+    addTaskToHTML(task.text, task.done);
+  }
+}
+initTasks();
+function addTaskObj(value) {
+  const newTask = { text: value, done: false, createdAt: Date.now(), deleted: false };
+  tasks.push(newTask);
+  writeToStorage();
+}
+function changeTask(index, data = {}) {
+  const task = tasks[index];
+
+  /* new */
+  const newTask = { ...task, ...data };
+  tasks[index] = newTask;
+
+  /* short */
+  // Object.assign(task, data);
+  writeToStorage();
+}
+function writeToStorage() {
+  const string = JSON.stringify(tasks);
+  localStorage.setItem('tasks', string);
+}
+
+
+/* ======================== */
+function addTaskToHTML(text, checked = false) {
   const task = document.createElement('li');
-  task.textContent = value;
+  const id = todoList.children.length;
+  task.dataset.id = id;
+
+  /* ======================== */
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = checked;
+  checkbox.addEventListener('change',
+  (event) => {
+    const checkbox = event.target;
+    const done = checkbox.checked;
+
+    changeTask(id, { done: done });
+  });
+  task.append(checkbox);
+
+  /* ======================== */
+
+  task.append(text);
+
+  /* ======================== */
+  const delBtn = document.createElement('button');
+  delBtn.append('x');
+  delBtn.addEventListener('click', (event) => {
+    changeTask(id, { deleted: true });
+    event.target.parentElement.remove();
+  });
+  task.append(delBtn);
+  /* ======================== */
+
   todoList.append(task);
 }
